d1ef8cf1be75686bd06a3c84c4f6f7a55a93c99b
Travis Walker <traviswalkerdev@gmail.com>
2017-10-20 19:50:22 -0700

    refactored out conditionals per the text

--- ch08_writing_own_methods/english_numbers.rb
+++ ch08_writing_own_methods/english_numbers.rb
@@ -1,77 +1,61 @@
 def englishNumber number
-  # We only want numbers from 0 - 100
-  if number < 0
-    return "Please enter a number zero or greater."
+  if number < 0   # No negative numbers
+    return "please enter a number that isn't negative."
   end
-  if number > 100
-    return "Please enter a number 100 or less."
+  if number == 0
+    return "zero"
   end
 
+
+
+  # No more special cases! No more returns!
 
   num_string = ""    # this is the string we will return
 
+  ones_place = ["one", "two", "three", "four", "five", "six", "seven", "eight",
+                "nine"]
+
+  tens_place = ["ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy",
+                "eighty", "ninety"]
+
+  teenagers = ["eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen",
+               "seventeen", "eighteen", "nineteen"]
+
+
+
   # "left" is how much of the number we still have left to write out
-  # "write" is the part we are writing out right now
+  # write is the part we are writing right now
 
   left = number
   write = left / 100             # How many hundreds left to write out
   left = left - (write * 100)    # Subtract off those hundreds
 
+
   if write > 0
-    return "one hundred"
+
+    # New trick here
+    hundreds = englishNumber write
+    num_string = num_string + hundreds + " hundred"
+
+    if left > 0
+      num_string = num_string + " "
+    end
   end
 
   write = left / 10              # How many tens left to write out
   left = left - (write * 10)     # Subract off those tens
 
   if write > 0
-    if write == 1  # Uh-oh...
-
-      # Since we can't write out "tenty-two" instead of "twelve",
-      # we need to make special exceptions for these
-
-      if left == 0
-        num_string = num_string + "ten"
-      elsif left == 1
-        num_string = num_string + "eleven"
-      elsif left == 2
-        num_string = num_string + "twelve"
-      elsif left == 3
-        num_string = num_string + "thirteen"
-      elsif left == 4
-        num_string = num_string + "fourteen"
-      elsif left == 5
-        num_string = num_string + "fifteen"
-      elsif left == 6
-        num_string = num_string + "sixteen"
-      elsif left == 7
-        num_string = num_string + "seventeen"
-      elsif left == 8
-        num_string = num_string + "eighteen"
-      elsif left == 9
-        num_string = num_string + "nineteen"
-      end
+    if ((write == 1) and (left > 0))
+      # Since we can't write "tenty-two" instead of "twelve",
+      # we have to make a special exception for these.
+      num_string = num_string + teenagers[left - 1]
 
       # Since we already took care of the digit in the ones place,
-      # we have nothing left ot write
-
+      # we have nothing left to write
       left = 0
-    elsif write == 2
-      num_string = num_string + "twenty"
-    elsif write == 3
-      num_string = num_string + "thirty"
-    elsif write == 4
-      num_string = num_string + "forty"
-    elsif write == 5
-      num_string = num_string + "fifty"
-    elsif write == 6
-      num_string = num_string + "sixty"
-    elsif write == 7
-      num_string = num_string + "seventy"
-    elsif write == 8
-      num_string = num_string + "eighty"
-    elsif write == 9
-      num_string = num_string + "ninety"
+    else
+      num_string = num_string + tens_place[write - 1]
     end
 
     if left > 0
@@ -83,33 +67,9 @@ def englishNumber number
   left = 0       # Subtract off those ones
 
   if write > 0
-    if write == 1
-      num_string = num_string + "one"
-    elsif write == 2
-      num_string = num_string + "two"
-    elsif write == 3
-      num_string = num_string + "three"
-    elsif write == 4
-      num_string = num_string + "four"
-    elsif write == 5
-      num_string = num_string + "five"
-    elsif write == 6
-      num_string == num_string + "six"
-    elsif write == 7
-      num_string = num_string + "seven"
-    elsif write == 8
-      num_string = num_string + "eight"
-    elsif write == 9
-      num_string = num_string + "nine"
-    end
+    num_string = num_string + ones_place[write - 1]
   end
 
-  if num_string == ""
-
-    # The only way num_string could be empty is if the number is zero
-
-    return "zero"
-  end
 
   num_string
 end
@@ -123,3 +83,8 @@ puts englishNumber(32)
 puts englishNumber(88)
 puts englishNumber(99)
 puts englishNumber(100)
+puts englishNumber(101)
+puts englishNumber(234)
+puts englishNumber(3211)
+puts englishNumber(999999)
+puts englishNumber(1000000000000)
