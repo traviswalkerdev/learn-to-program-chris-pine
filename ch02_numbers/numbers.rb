5d62a67fc149fa0015a3a3047acfa040b73da740
Travis Walker <traviswalkerdev@gmail.com>
2017-10-23 18:42:57 -0700

    Updated numbers.rb programs to account for leap years

--- ch02_numbers/numbers.rb
+++ ch02_numbers/numbers.rb
@@ -1,9 +1,10 @@
 def hours_in_a_year
-  puts 365 * 24
+  puts "#{365 * 24}, or #{366 * 24} for leap years"
 end
 
 def minutes_in_a_decade
-  puts 10 * 365 * 24 * 60
+  puts "#{((10 * 365) + 2) * 24 * 60} or #{((10 * 365) + 3) * 24 * 60},"
+  puts "depending on leap years in the particular decade."
 end
 
 def age_in_seconds(age)
@@ -19,7 +20,7 @@ puts "-" * 10
 puts
 
 def seconds_to_age(seconds)
-  puts seconds /365 /24 /60 /60
+  puts seconds / (365 * 24 * 60 * 60)
 end
 
 seconds_to_age(1160000000)
