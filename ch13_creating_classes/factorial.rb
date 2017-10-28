5b2ada9b8a1da6071fe43fb65402bc9cd23f5023
Travis Walker <traviswalkerdev@gmail.com>
2017-10-28 10:42:31 -0700

    ch13 extend Integer class with factorial

---
+++ ch13_creating_classes/factorial.rb
@@ -0,0 +1,24 @@
+class Integer
+
+  def factorial
+    number = self
+    if self < 0
+      return "You can't take the factorial of a negative number."
+    end
+
+    if self <= 1
+      1
+    else
+      self * (self - 1).factorial
+    end
+
+  end
+
+end
+
+
+
+puts 5.factorial
+puts 8.factorial
+puts 12.factorial
+puts 3.factorial
